36a486c614a3cf665b76db24c75f127bb2baebbd
Guilherme Diniz <guilhermediniz11@live.com>
2017-04-05 00:21:33 -0300

    Fazendo chatbot funcionar basicamente

--- server.js
+++ server.js
@@ -7,23 +7,49 @@ let request = require('request-promise');
 
 // These are the MessagingHub credentials for this bot.
 // If you want to create your own bot, see http://blip.ai
-const IDENTIFIER = 'sdkbreno';
-const ACCESS_KEY = 'OENvWVRtWUJUN2JjYWQ3S2xUSGI=';
+const IDENTIFIER = 'autozinho';
+const ACCESS_KEY = 'bml1d1FSaVVuQWRHQklFQ1J3cEw=';
 
-// Cria uma instância do cliente, informando o identifier e accessKey do seu chatbot 
+// Cria uma instância do cliente, informando o identifier e accessKey do seu chatbot
 var client = new MessagingHub.ClientBuilder()
-    .withIdentifier(identifier)
-    .withAccessKey(accessKey)
+    .withIdentifier(IDENTIFIER)
+    .withAccessKey(ACCESS_KEY)
     .withTransportFactory(() => new WebSocketTransport())
     .build();
 
 // Registra um receiver para mensagens do tipo 'text/plain'
 client.addMessageReceiver('text/plain', function(message) {
-  // TODO: Processe a mensagem recebida
+  console.log("OLA");
+  console.log(message);
+  var uri = "lime://messenger.gw.msging.net/accounts/" + message.from.split("@")[0]
+  console.log(uri);
+
+//OS COMANDOS TEM QUE PASSAR O ID DE ACORDO COM OS DOCS
+  var command = {
+    id: 1,
+    uri: uri,
+    method: Lime.CommandMethod.GET,
+    to: "postmaster@messenger.gw.msging.net"
+  };
+
+  client.sendCommand(command)
+    .then(function(response) {
+      var msg = {
+        id: Lime.Guid(),
+        type: "text/plain",
+        content: "Olá "+ response.resource.fullName +", em que posso ajudar?",
+        to: message.from
+      };
+      client.sendMessage(msg);
+    }).catch((err) => console.error(err));
+
+      // send a message to some user
+
 });
 
 client.addMessageReceiver('application/json', function(message) {
-  // TODO: Processe a mensagem recebida
+  console.log("OLA2");
+  console.log(message);
 });
 
 // Registra um receiver para qualquer notificação
@@ -31,7 +57,7 @@ client.addNotificationReceiver(true, function(notification) {
   // TODO: Processe a notificação recebida
 });
 
-// Conecta com o servidor de forma assíncrona. 
+// Conecta com o servidor de forma assíncrona.
 // A conexão ocorre via websocket, na porta 8081.
 client.connect()
     .then(() => console.log('Listening...'))
